afe5447604f2fffbfa83a14c1f037c1ef21be7ff
kenji hajiro <mademoiselle3824@gmail.com>
2022-02-09 13:39:52 +0900

    scaffold article

---
+++ db/migrate/20220209043856_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :link
+      t.text :icon
+      t.integer :ranking
+
+      t.timestamps
+    end
+  end
+end

8e6c7ab99d18ccb86edbac92da64c7c095b5c7cd
kenji hajiro <mademoiselle3824@gmail.com>
2022-02-16 00:35:13 +0900

    modelテスト　最低限

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,19 @@ module WhatInteresting
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       model_specs: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: false,
+                       request_specs: false
+    end
+
+    config.action_mailer.default_url_options = { host: 'example.com' }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

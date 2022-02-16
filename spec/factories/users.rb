212b483f4e31845eaf6d467cb06c765c42470988
kenji hajiro <mademoiselle3824@gmail.com>
2022-02-16 15:51:48 +0900

    systemテスト　最低限

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,10 @@
 FactoryBot.define do
   factory :user do
     name { 'dddd' + rand(1..10).to_s }
-    email { Faker::Internet.email }
+    email { 'asd@asd.com' }
     password              { 'dddddd' }
     password_confirmation { 'dddddd' }
     admin                 { false }
+    # after(:create) { |user| user.confirm }
   end
 end

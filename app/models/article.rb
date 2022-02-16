ccca284d4a4d69b80da559a820d4e802e4e1a1b7
kenji hajiro <mademoiselle3824@gmail.com>
2022-02-16 16:29:41 +0900

    退会ボタンとrspec

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
-  has_one :article_statistic
-  has_many :comments
+  has_one :article_statistic, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   # 誤って外部から保存されるのを防ぐため意味わからんバリテーション設定
   validates :title, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }

61a2717884021733a59e4bd6c962c506278184f0
kenji hajiro <mademoiselle3824@gmail.com>
2022-02-22 11:33:43 +0900

    エラーメッセージデザイン、不要ボタン削除

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,6 @@ module WhatInteresting
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    config.i18n.default_locale = :ja
-
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 

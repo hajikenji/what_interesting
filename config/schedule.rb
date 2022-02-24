995897d1b35ee840916287c7c26b46958f461001
kenji hajiro <mademoiselle3824@gmail.com>
2022-02-24 21:13:03 +0900

    wheneverにスクレイピング設定

--- config/schedule.rb
+++ config/schedule.rb
@@ -9,7 +9,30 @@ set :output, "#{Rails.root}/log/cron.log"
 # ジョブの実行環境の指定
 set :environment, :development
 
+# yahoo
+# url = "https://news.yahoo.co.jp/topics/top-picks?page=#{params[:name].to_i}"
+def url(page_number)
+  "https://news.yahoo.co.jp/topics/top-picks?page=#{page_number}"
+end
+
 # 3時間毎に実行するスケジューリング
 every 10.minutes do
   runner "ArticleStatistic.whenever_test"
+end
+every 15.minutes do
+  runner "ArticleStatistic.whenever_test"
+end
+
+every 1.hours do
+  runner "ArticleStatistic.scraping_yahoo(#{url(1)})"
+end
+every 1.hours do
+  sleep(600)
+  runner "ArticleStatistic.scraping_yahoo(#{url(2)})"
+end
+every 2.hours do
+  runner "ArticleStatistic.scraping_yahoo(#{url(3)})"
+end
+every 3.hours do
+  runner "ArticleStatistic.scraping_yahoo(#{url(4)})"
 end
